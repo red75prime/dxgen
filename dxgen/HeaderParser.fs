8483cad38022a91fb7ffdace8edace4a11cc3967
Mr. Byte <bytemr@gmail.com>
2014-11-16 11:01:24 -0700

    Start work on abstracting libclang into a safer and more functional interface.

---
+++ dxgen/HeaderParser.fs
@@ -0,0 +1,44 @@
+﻿module HeaderParser
+
+open System.Runtime.InteropServices
+open libclang
+
+type ASTNode = {
+    Kind: CursorKind
+    Name: string
+    Children: ASTNode list
+}
+
+let private buildNode kind name children = {
+        Kind = kind
+        Name = name
+        Children = children
+    }
+
+let private extractString (str: String) =
+    let result = System.Runtime.InteropServices.Marshal.PtrToStringAnsi(str.data)
+    libclang.disposeString(str)
+    result
+
+let rec private visitChildren (cursor: Cursor) (parent: Cursor) (clientData: ClientData): ChildVisitResult =
+    let mutable handle = clientData |> GCHandle.FromIntPtr
+    let childrenHandle = ([]: ASTNode list) |> GCHandle.Alloc
+
+    (cursor, new CursorVisitor(visitChildren), childrenHandle |> GCHandle.ToIntPtr) |> libclang.visitChildren |> ignore
+ 
+    let result = { 
+        Kind = cursor |> getCursorKind
+        Name = cursor |> getCursorSpelling |> extractString
+        Children =  (childrenHandle.Target :?> list<ASTNode>)
+    }
+
+    handle.Target <- match handle.Target with
+                     | :? list<ASTNode> as siblings ->
+                        result :: siblings :> obj
+                     | :? ASTNode as root ->
+                        { root with Children = result :: root.Children } :> obj
+                     | _ -> failwith("Unexpected type in node traversal.")
+    
+    childrenHandle.Free()
+
+    libclang.ChildVisitResult.Continue
