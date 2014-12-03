4d9e2c824c4e72b59292f89e22ab79e1b635c4f6
Mr. Byte <bytemr@gmail.com>
2014-12-02 21:26:17 -0700

    Added a module for testing HeaderConverter.

---
+++ dxgen.test/HeaderConverterTests.fs
@@ -0,0 +1,9 @@
+﻿module HeaderConverterTests
+
+open HeaderConverter
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Converting a header containing an enum shield produce a CodeGenTree with the contents of the enum`` () =
+    failwith "Not implemented"
