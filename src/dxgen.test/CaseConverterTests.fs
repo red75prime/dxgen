84dfa12779f1a4006981d70fb1b41d4c9dea3b8c
Mr. Byte <bytemr@gmail.com>
2014-12-09 21:32:38 -0700

    Added a CaseConverter module to handle conversion of identifier cases.

---
+++ src/dxgen.test/CaseConverterTests.fs
@@ -0,0 +1,22 @@
+﻿module CaseConverterTests
+
+open CaseConverter
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Converting pascal case to snake case should produce an identifier that has snake case`` () =
+    "TestIdentifier" |> toSnakeCase |> should equal "test_identifier"
+
+[<Test>]
+let ``Converting enum case to snake case should produce an identifier that has snake case`` () =
+    "TEST_IDENTIFIER" |> toSnakeCase |> should equal "test_identifier"
+
+
+[<Test>]
+let ``Converting pascal case to pascale case should produce an identifier that has pascal case`` () =
+    "TestIdentifier" |> toPascalCase |> should equal "TestIdentifier"
+
+[<Test>]
+let ``Converting enum case to pascal case should produce an identifier that has pascal case`` () =
+    "TEST_IDENTIFIER" |> toPascalCase |> should equal "TestIdentifier"
---
+++ src/dxgen/CaseConverter.fs
@@ -0,0 +1,30 @@
+﻿module CaseConverter
+
+open System
+open System.Text.RegularExpressions
+open FSharpx
+
+type private PascalCaseMatcher = Regex< @"^(?!.*_)(?<Identifier>[A-Z0-9][a-z0-9]*)+" >
+type private EnumCaseMatcher = Regex< @"^(?![A-Z0-9]*[a-z])((?<Identifier>[A-Z0-9]+)_?)+" >
+    
+let private getIdentifierParts ident  =
+    let matchGroup = 
+        match ident with
+        | _ when PascalCaseMatcher.IsMatch(ident) -> PascalCaseMatcher().Match(ident).Identifier
+        | _ when EnumCaseMatcher.IsMatch(ident) -> EnumCaseMatcher().Match(ident).Identifier
+        | _ -> failwith "Unexpected identifier case."
+
+    matchGroup.Captures |> Linq.Enumerable.OfType |> Seq.map(fun (c: Capture) -> c.Value)
+
+let public toSnakeCase ident =
+    ident
+    |> getIdentifierParts
+    |> Seq.map (fun part -> part.ToLower())
+    |> (fun parts -> String.Join("_", parts))
+
+let public toPascalCase ident =
+    ident
+    |> getIdentifierParts
+    |> Seq.map (fun part -> System.Globalization.CultureInfo.InvariantCulture.TextInfo.ToTitleCase(part.ToLower())) 
+    |> (fun parts -> String.Join("", parts))
+                        
