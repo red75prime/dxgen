1e172f8776c2530302f02669df93e1d2fed894c5
Mr. Byte <bytemr@gmail.com>
2014-12-11 20:32:43 -0700

    Some code cleanup on the HeaderConverter module.

--- src/dxgen/HeaderConverter.fs
+++ src/dxgen/HeaderConverter.fs
@@ -5,13 +5,13 @@ open HeaderLoader
 
 let toHeaderTypeInfo (headerRoot: Node): HeaderTypeInfo =
     let parseEnum (enumParent: Node) =
-        let rec parseVariants (accum: EnumVariant list) (nodes: Node list) =
-            let parseVariant (node: Node) =                
-                match node with
-                | { Info = NodeInfo(libclang.CursorKind.EnumConstantDecl, name); Value = Some(EnumValue(value)) } -> EnumVariant(name, value)
-                | _ -> failwith "Unexpected enum variant."
+        let parseVariant =                
+            function
+            | { Info = NodeInfo(libclang.CursorKind.EnumConstantDecl, name); Value = Some(EnumValue(value)) } -> EnumVariant(name, value)
+            | _ -> failwith "Unexpected enum variant."
                 
-            match nodes with
+        let rec parseVariants (accum: EnumVariant list) =
+            function
             | [] -> accum |> List.rev
             | node :: nodes -> nodes |> parseVariants ((node |> parseVariant) :: accum)
 
@@ -19,31 +19,34 @@ let toHeaderTypeInfo (headerRoot: Node): HeaderTypeInfo =
         Enum(name, enumParent.Children |> parseVariants [])
 
     let parseStruct (structParent: Node) =
-        let parseArrayBounds (nodes: Node list) =
-            if nodes.IsEmpty then
-                None
-            else
-                nodes 
-                |> List.map (fun node -> match node with
-                                         | { Info = NodeInfo(libclang.CursorKind.IntegerLiteral, _); Value = Some(LiteralValue(value)) } -> value |> System.Convert.ToUInt64
-                                         | _ -> failwith "Unexpected literal value.") 
-                |> Some
+        let parseArrayBounds =
+            function
+            | [] -> None
+            | nodes -> nodes 
+                       |> List.map (function
+                                    | { Info = NodeInfo(libclang.CursorKind.IntegerLiteral, _); Value = Some(LiteralValue(value)) } -> value |> System.Convert.ToUInt64
+                                    | _ -> failwith "Unexpected literal value.") 
+                       |> Some
 
-        let rec parseFields (accum: StructField list) (nodes: Node list) =
-            let parseField (node: Node) =
-                match node with
-                | { Info = NodeInfo(libclang.CursorKind.FieldDecl, name); Type = Some(NodeType(tu, typeName)) } -> StructField(typeName, name, node.Children |> parseArrayBounds)
-                | _ -> failwith "Unexpected struct field."
+        let parseField =
+            function
+            | { Info = NodeInfo(libclang.CursorKind.FieldDecl, name); Type = Some(NodeType(_, typeName)) } as node -> 
+                StructField(typeName, name, node.Children |> parseArrayBounds)
+            | _ -> failwith "Unexpected struct field."
 
-            match nodes with
+        let rec parseFields (accum: StructField list) =
+            function
             | [] -> accum |> List.rev
             | node :: nodes -> nodes |> parseFields ((node |> parseField) :: accum)
 
         let (NodeInfo(_, name)) = structParent.Info
         Struct(name, structParent.Children |> parseFields [])
 
-    let rec toHeaderTypeInfo (accum: HeaderTypeInfo) (nodes: Node list) =
-        match nodes with
+    let rec parseInterface (interfaceParent: Node) =
+        ()
+
+    let rec toHeaderTypeInfo (accum: HeaderTypeInfo) =
+        function
         | [] -> accum
         | node :: nodes -> 
             match node.Info with
