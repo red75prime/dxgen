0db42999a1113f039fe19b8e1cd56a09af35f2ba
Mr. Byte <bytemr@gmail.com>
2014-12-12 20:57:50 -0700

    Some changes to CaseConverter to deal with possible case ambiguities.

--- src/dxgen.test/CaseConverterTests.fs
+++ src/dxgen.test/CaseConverterTests.fs
@@ -6,21 +6,21 @@ open FsUnit
 
 [<Test>]
 let ``Converting pascal case to snake case should produce an identifier that has snake case`` () =
-    "TestIdentifier" |> toSnakeCase |> should equal "test_identifier"
+    "TestIdentifier" |> toSnakeCase SourceCase.Pascal |> should equal "test_identifier"
 
 [<Test>]
 let ``Converting enum case to snake case should produce an identifier that has snake case`` () =
-    "TEST_IDENTIFIER" |> toSnakeCase |> should equal "test_identifier"
+    "TEST_IDENTIFIER" |> toSnakeCase SourceCase.Enum |> should equal "test_identifier"
 
 
 [<Test>]
 let ``Converting pascal case to pascal case should produce an identifier that has pascal case`` () =
-    "TestIdentifier" |> toPascalCase |> should equal "TestIdentifier"
+    "TestIdentifier" |> toPascalCase SourceCase.Pascal |> should equal "TestIdentifier"
 
 [<Test>]
 let ``Converting enum case to pascal case should produce an identifier that has pascal case`` () =
-    "TEST_IDENTIFIER" |> toPascalCase |> should equal "TestIdentifier"
+    "TEST_IDENTIFIER" |> toPascalCase SourceCase.Enum |> should equal "TestIdentifier"
 
 [<Test>]
 let ``Converting single word enum case to pascal case should produce an identifier that has pascal case`` () =
-    "IDENTIFIER" |> toPascalCase |> should equal "Identifier"
+    "IDENTIFIER" |> toPascalCase SourceCase.Enum |> should equal "Identifier"
--- src/dxgen/CaseConverter.fs
+++ src/dxgen/CaseConverter.fs
@@ -4,27 +4,31 @@ open System
 open System.Text.RegularExpressions
 open FSharpx
 
-type private PascalCaseMatcher = Regex< @"^(?!.*_)(?<Identifier>[A-Z][a-z0-9]*)(?<Identifier>[A-Z0-9][a-z0-9]*)*" >
-type private EnumCaseMatcher = Regex< @"^(?![A-Z0-9]*_?[^A-Z0-9_])((?<Identifier>[A-Z0-9]+)_?)+" >
+type private PascalCaseMatcher = Regex< @"^(?<Identifier>[A-Z0-9][a-z0-9]*)+" >
+type private EnumCaseMatcher = Regex< @"((?<Identifier>[A-Z0-9]+)_?)+" >
+  
+type public SourceCase =
+    | Pascal = 0
+    | Enum = 1
     
-let private getIdentifierParts ident  =
+let private getIdentifierParts sourceCase ident =
     let matchGroup = 
-        match ident with
-        | _ when PascalCaseMatcher.IsMatch(ident) -> PascalCaseMatcher().Match(ident).Identifier
-        | _ when EnumCaseMatcher.IsMatch(ident) -> EnumCaseMatcher().Match(ident).Identifier
+        match sourceCase with
+        | SourceCase.Pascal -> PascalCaseMatcher().Match(ident).Identifier
+        | SourceCase.Enum -> EnumCaseMatcher().Match(ident).Identifier
         | _ -> failwith "Unexpected identifier case."
 
     matchGroup.Captures |> Linq.Enumerable.OfType |> Seq.map(fun (c: Capture) -> c.Value)
 
-let public toSnakeCase ident =
+let public toSnakeCase sourceCase ident =
     ident
-    |> getIdentifierParts
+    |> getIdentifierParts sourceCase
     |> Seq.map (fun part -> part.ToLower())
     |> (fun parts -> String.Join("_", parts))
 
-let public toPascalCase ident =
+let public toPascalCase sourceCase ident =
     ident
-    |> getIdentifierParts
+    |> getIdentifierParts sourceCase
     |> Seq.map (fun part -> System.Globalization.CultureInfo.InvariantCulture.TextInfo.ToTitleCase(part.ToLower())) 
     |> (fun parts -> String.Join("", parts))
                         
