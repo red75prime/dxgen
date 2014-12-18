550af7e02d2f94e77c57e8b20d8dd5914966c445
Mr. Byte <bytemr@gmail.com>
2014-12-17 20:04:20 -0700

    Document how the ModuleBuilder will potentially work.

--- src/dxgen/ModuleBuilder.fs
+++ src/dxgen/ModuleBuilder.fs
@@ -1,2 +1,12 @@
 ﻿module ModuleBuilder
 
+open HeaderInfo
+
+type EnumBuilder = Enum -> string
+type StructBuilder = Struct -> string
+type InterfaceBuilder = Interface -> string
+
+//Builder will generate a lib.rs which will re-export all the other files generated.
+//Builder will generate one file per enum, struct, and interface.
+//Builder will potentially perform name transformations to make the names more idiomatic rust.
+//Builder will need to translate several types into rust equivalents.
