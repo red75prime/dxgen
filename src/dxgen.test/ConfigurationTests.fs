5e6ba822bd70a95925e8302924fd502d78a3a5b0
Mr. Byte <bytemr@gmail.com>
2014-12-23 14:24:50 -0700

    More work on loading configurations from YAML files.

---
+++ src/dxgen.test/ConfigurationTests.fs
@@ -0,0 +1,36 @@
+﻿module ConfigurationTests
+
+open NUnit.Framework
+open FsUnit
+
+open Configuration
+
+let testConfig = 
+    """---
+    modules:
+        - name: dx_dxgi
+          output-location: ../test/
+          headers:
+              - dxgi_types.h
+              - dxgi.h
+    """
+
+[<Test>]
+let ``loadConfiguration should return the number of expected modules`` () =
+    let config = testConfig
+                 |> (fun s -> new System.IO.StringReader(s))
+                 |> loadConfiguration
+
+    config.Modules |> should haveCount 1
+
+[<Test>]  
+let ``loadConfiguration should contain a module with the expected name`` () =
+    let config = testConfig
+                 |> (fun s -> new System.IO.StringReader(s))
+                 |> loadConfiguration
+
+    (config.Modules 
+     |> Seq.exactlyOne).Name
+    |> should equal "dx_dxgi"
+   
+   
---
+++ src/dxgen/Configuration.fs
@@ -0,0 +1,27 @@
+﻿module Configuration
+
+open SharpYaml.Serialization
+
+[<CLIMutable>]
+type Module = {
+    [<YamlMember("name")>]
+    Name : string
+
+    [<YamlMember("headers")>]
+    Headers : string seq
+
+    [<YamlMember("output-location")>]
+    OutputLocation : string
+}
+
+[<CLIMutable>]
+type Configuration = {
+    [<YamlMember("modules")>]
+    Modules : Module seq
+}
+with
+    override this.ToString() = sprintf "%A" this
+
+let loadConfiguration (textReader: System.IO.TextReader) =
+    let deserializer = Serializer()
+    deserializer.Deserialize<Configuration>(textReader)
