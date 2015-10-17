93ec75e3f8bcd5e20f4693d48806e6d2abaf2749
red75prime <red75prim@gmail.com>
2015-10-17 12:24:03 +0500

    Cleanup

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-".\tools\nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-
-".\tools\nuget\NuGet.exe" "Install" "NUnit.Runners" "-OutputDirectory" "packages" "-ExcludeVersion"
-
-"packages\FAKE\tools\Fake.exe" build.fsx
