461d798debc3db2e3d84c768ee7ae51e3aea3c6e
red75prime <red75prim@gmail.com>
2016-05-25 17:48:22 +0500

    Minor tweaks

--- src/dxgen/scaffolding/src/cubestate.rs
+++ src/dxgen/scaffolding/src/cubestate.rs
@@ -6,6 +6,7 @@ use utils::v3;
 
 pub type V3 = Vector3<f32>;
 
+#[derive(Clone)]
 pub struct CubeState {
     pub pos: V3,
     pub rot: Basis3<f32>,
@@ -14,6 +15,7 @@ pub struct CubeState {
     pub rot_spd: f32,
 }
 
+#[derive(Clone)]
 pub struct State {
     pub tick: f64,
     pub cubes: Vec<CubeState>,
@@ -50,63 +52,68 @@ impl State {
         }
     }
 
+    fn update_one(cube: &CubeState, dt: f32) -> CubeState {
+        let mut new_spd = cube.spd;
+        let mut new_pos = cube.pos + cube.spd * dt;
+        if new_pos.x < -CUBE_SPAN/2. {
+            new_pos.x = -CUBE_SPAN/2.;
+            new_spd.x = -new_spd.x;
+        }
+        if new_pos.x > CUBE_SPAN/2. {
+            new_pos.x = CUBE_SPAN/2.;
+            new_spd.x = -new_spd.x;
+        }
+        if new_pos.y < -CUBE_SPAN/2. {
+            new_pos.y = -CUBE_SPAN/2.;
+            new_spd.y = -new_spd.y;
+        }
+        if new_pos.y > CUBE_SPAN/2. {
+            new_pos.y = CUBE_SPAN/2.;
+            new_spd.y = -new_spd.y;
+        }
+        if new_pos.z < -CUBE_SPAN {
+            new_pos.z = -CUBE_SPAN;
+            new_spd.z = -new_spd.z;
+        }
+        if new_pos.z > 0. {
+            new_pos.z = 0.;
+            new_spd.z = -new_spd.z;
+        }
+        CubeState {
+            pos: new_pos,
+            spd: new_spd,
+            rot: Basis3::from_axis_angle(cube.rot_axe, deg(cube.rot_spd * dt).into()).concat(&cube.rot),
+            .. *cube
+        }
+    }
+
     pub fn update(&self, dt: f32, thread_cnt: u32) -> Self {
+        if dt == 0.0 {
+          return (*self).clone();
+        }
         // advance position of cubes
         let mut cubes = Vec::with_capacity(self.cubes.len());
-        // Uninitialized content of cubes will be immediately overwritten
-        unsafe{ cubes.set_len(self.cubes.len()) };
-        crossbeam::scope(|scope| {
-            let chunk_len = (cubes.len()/thread_cnt as usize)+1;
-            for (chunk, chunk_src) in cubes[..].chunks_mut(chunk_len).zip(self.cubes[..].chunks(chunk_len)) {
-                scope.spawn(move || {
-                    for (c, cube) in chunk.iter_mut().zip(chunk_src) {
-                        let mut new_spd = cube.spd;
-                        let mut new_pos = cube.pos + cube.spd * dt;
-                        if new_pos.x < -CUBE_SPAN/2. {
-                            new_pos.x = -CUBE_SPAN/2.;
-                            new_spd.x = -new_spd.x;
-                        }
-                        if new_pos.x > CUBE_SPAN/2. {
-                            new_pos.x = CUBE_SPAN/2.;
-                            new_spd.x = -new_spd.x;
-                        }
-                        if new_pos.y < -CUBE_SPAN/2. {
-                            new_pos.y = -CUBE_SPAN/2.;
-                            new_spd.y = -new_spd.y;
-                        }
-                        if new_pos.y > CUBE_SPAN/2. {
-                            new_pos.y = CUBE_SPAN/2.;
-                            new_spd.y = -new_spd.y;
-                        }
-                        if new_pos.z < -CUBE_SPAN {
-                            new_pos.z = -CUBE_SPAN;
-                            new_spd.z = -new_spd.z;
-                        }
-                        if new_pos.z > 0. {
-                            new_pos.z = 0.;
-                            new_spd.z = -new_spd.z;
-                        }
-                        *c = CubeState {
-                            pos: new_pos,
-                            spd: new_spd,
-                            rot: Basis3::from_axis_angle(cube.rot_axe, Angle::from(deg(cube.rot_spd * dt))).concat(&cube.rot),
-                            .. *cube
+        if thread_cnt == 1 {
+            for cube in &self.cubes[..] {
+                cubes.push(State::update_one(cube, dt));
+            }
+        } else {
+            // Uninitialized content of cubes will be immediately overwritten
+            unsafe{ cubes.set_len(self.cubes.len()) };
+            crossbeam::scope(|scope| {
+                let chunk_len = (cubes.len()/thread_cnt as usize)+1;
+                for (chunk, chunk_src) in cubes[..].chunks_mut(chunk_len).zip(self.cubes[..].chunks(chunk_len)) {
+                    scope.spawn(move || {
+                        for (c, cube) in chunk.iter_mut().zip(chunk_src) {
+                            *c = State::update_one(cube, dt);
                         };
-                    };
-                });
-            };
-        });
-         
-//        for cube in &self.cubes {
-//            let mut cs = CubeState {
-//                pos: cube.pos + cube.spd * dt,
-//                rot: Basis3::from_axis_angle(cube.rot_axe, Angle::from(deg(cube.rot_spd * dt))).concat(&cube.rot),
-//                .. *cube};
-//            cubes.push(cs);
-//        }
+                    });
+                };
+            });
+        }
         let (lx, ly) = f64::sin_cos(self.tick*0.1);
         let (lx, ly) = (lx as f32, ly as f32);
-        let light_pos = v3(lx*50., ly*50., -3.);
+        let light_pos = v3(0., 0., -100.);//v3(lx*50., ly*50., -201.);
         State {
             tick: self.tick + dt as f64,
             cubes: cubes,
